e93785c8fb4082dbe11531e06f5ef7c8838d6248
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-02-06 19:32:49 +0300

    fix typo

--- src/app/showcase/doc/progressspinner/importdoc.ts
+++ src/app/showcase/doc/progressspinner/importdoc.ts
@@ -15,6 +15,6 @@ export class ImportDocComponent {
 
     code: Code = {
         typescript: `
-import {ProgressSpinnerModule} from 'primeng/progressspinner';`
+import { ProgressSpinnerModule } from 'primeng/progressspinner';`
     };
 }
