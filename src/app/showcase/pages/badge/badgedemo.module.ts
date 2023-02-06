71f493c967b3883fbed382ee8598ac0173428982
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-02-06 15:17:20 +0300

    Remove unused imports

--- src/app/showcase/pages/badge/badgedemo.module.ts
+++ src/app/showcase/pages/badge/badgedemo.module.ts
@@ -1,18 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { BadgeModule } from 'primeng/badge';
-import { ButtonModule } from 'primeng/button';
-import { PanelModule } from 'primeng/panel';
-import { TabViewModule } from 'primeng/tabview';
-import { AppCodeModule } from 'src/app/showcase/layout/doc/code/app.code.component';
-import { AppDemoActionsModule } from '../../layout/demoactions/app.demoactions.component';
 import { BadgeDemo } from './badgedemo';
 import { AppDocModule } from '../../layout/doc/app.doc.module';
 import { BadgeDocModule } from '../../doc/badge/badgedoc.module';
 import { BadgeDemoRoutingModule } from './badgedemo-routing.module';
 
 @NgModule({
-    imports: [CommonModule, BadgeDemoRoutingModule, ButtonModule, PanelModule, TabViewModule, BadgeModule, AppDemoActionsModule, AppCodeModule, AppDocModule, BadgeDocModule],
+    imports: [CommonModule, BadgeDemoRoutingModule, AppDocModule, BadgeDocModule],
     declarations: [BadgeDemo]
 })
 export class BadgeDemoModule {}
--- src/app/showcase/pages/blockui/blockuidemo.module.ts
+++ src/app/showcase/pages/blockui/blockuidemo.module.ts
@@ -1,18 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { BlockUIModule } from 'primeng/blockui';
-import { ButtonModule } from 'primeng/button';
-import { PanelModule } from 'primeng/panel';
-import { TabViewModule } from 'primeng/tabview';
-import { AppCodeModule } from 'src/app/showcase/layout/doc/code/app.code.component';
 import { BlockUIDocModule } from '../../doc/blockui/blockuidoc.module';
-import { AppDemoActionsModule } from '../../layout/demoactions/app.demoactions.component';
 import { AppDocModule } from '../../layout/doc/app.doc.module';
 import { BlockUIDemo } from './blockuidemo';
 import { BlockUIDemoRoutingModule } from './blockuidemo-routing.module';
 
 @NgModule({
-    imports: [CommonModule, BlockUIDemoRoutingModule, BlockUIModule, ButtonModule, PanelModule, TabViewModule, AppCodeModule, AppDemoActionsModule, BlockUIDocModule, AppDocModule],
+    imports: [CommonModule, BlockUIDemoRoutingModule, BlockUIDocModule, AppDocModule],
     declarations: [BlockUIDemo]
 })
 export class BlockUIDemoModule {}
