45a511389af6442a369d52918c30f29bc2149947
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-02-06 16:43:05 +0300

    refactor

--- src/app/showcase/pages/inputtext/inputtextdemo.module.ts
+++ src/app/showcase/pages/inputtext/inputtextdemo.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
-import { AppCodeModule } from 'src/app/showcase/layout/doc/code/app.code.component';
 import { InputtextDocModule } from '../../doc/inputtext/inputtextdoc.module';
 import { AppDocModule } from '../../layout/doc/app.doc.module';
 
@@ -10,7 +9,7 @@ import { InputTextDemo } from './inputtextdemo';
 import { InputTextDemoRoutingModule } from './inputtextdemo-routing.module';
 
 @NgModule({
-    imports: [CommonModule, InputTextDemoRoutingModule, FormsModule, InputTextModule, AppCodeModule, InputtextDocModule, AppDocModule],
+    imports: [CommonModule, InputTextDemoRoutingModule, FormsModule, InputTextModule, InputtextDocModule, AppDocModule],
     declarations: [InputTextDemo]
 })
 export class InputTextDemoModule {}
