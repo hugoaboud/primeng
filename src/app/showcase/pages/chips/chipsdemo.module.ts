e1b3b3ed67f39b5aed18b060f41551c00bfd162f
gucal <gucalaliriza@outlook.com>
2023-02-07 11:36:36 +0300

    Refactor

--- src/app/showcase/pages/chips/chipsdemo.module.ts
+++ src/app/showcase/pages/chips/chipsdemo.module.ts
@@ -1,16 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { ButtonModule } from 'primeng/button';
 import { ChipsModule } from 'primeng/chips';
 import { ChipsDocModule } from '../../doc/chips/chipsdoc.module';
-import { AppDemoActionsModule } from '../../layout/demoactions/app.demoactions.component';
 import { AppDocModule } from '../../layout/doc/app.doc.module';
 import { ChipsDemo } from './chipsdemo';
 import { ChipsDemoRoutingModule } from './chipsdemo-routing.module';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, ChipsDemoRoutingModule, ChipsModule, ButtonModule, AppDemoActionsModule, AppDocModule, ChipsDocModule],
+    imports: [CommonModule, ChipsDemoRoutingModule, ChipsModule, AppDocModule, ChipsDocModule],
     declarations: [ChipsDemo]
 })
 export class ChipsDemoModule {}
