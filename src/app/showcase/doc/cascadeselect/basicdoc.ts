58802f1f38a17f1d2426abd3a520d211828d6ac9
gucal <gucalaliriza@outlook.com>
2023-02-13 12:17:10 +0300

    Add basicdoc for cascadeselect

--- src/app/showcase/doc/cascadeselect/basicdoc.ts
+++ src/app/showcase/doc/cascadeselect/basicdoc.ts
@@ -103,91 +103,101 @@ export class BasicDocComponent {
     }
 
     code: Code = {
+        basic: `
+<p-cascadeSelect [(ngModel)]="selectedCity" [options]="countries" optionLabel="cname" optionGroupLabel="name" [optionGroupChildren]="['states', 'cities']" [style]="{ minWidth: '14rem' }" placeholder="Select a City"></p-cascadeSelect>`,
+
         html: `
-<p-cascadeSelect [(ngModel)]="selectedCity" [options]="countries" optionLabel="cname" optionGroupLabel="name"
-    [optionGroupChildren]="['states', 'cities']" [style]="{ minWidth: '14rem' }" 
-    placeholder="Select a City"></p-cascadeSelect>`,
+<div class="card flex justify-content-center">
+    <p-cascadeSelect [(ngModel)]="selectedCity" [options]="countries" optionLabel="cname" optionGroupLabel="name" [optionGroupChildren]="['states', 'cities']" [style]="{ minWidth: '14rem' }" placeholder="Select a City"></p-cascadeSelect>
+</div>`,
+
         typescript: `
-countries: any[];
+import { Component } from '@angular/core';
 
-selectedCity: any;
+@Component({
+    templateUrl: './cascadeselectdemo.html'
+})
+export class CascadeSelectDemo {
+    countries: any[];
 
-ngOnInit() {
-    this.countries = [
-        {
-            name: 'Australia',
-            code: 'AU',
-            states: [
-                {
-                    name: 'New South Wales',
-                    cities: [
-                        { cname: 'Sydney', code: 'A-SY' },
-                        { cname: 'Newcastle', code: 'A-NE' },
-                        { cname: 'Wollongong', code: 'A-WO' }
-                    ]
-                },
-                {
-                    name: 'Queensland',
-                    cities: [
-                        { cname: 'Brisbane', code: 'A-BR' },
-                        { cname: 'Townsville', code: 'A-TO' }
-                    ]
-                }
-            ]
-        },
-        {
-            name: 'Canada',
-            code: 'CA',
-            states: [
-                {
-                    name: 'Quebec',
-                    cities: [
-                        { cname: 'Montreal', code: 'C-MO' },
-                        { cname: 'Quebec City', code: 'C-QU' }
-                    ]
-                },
-                {
-                    name: 'Ontario',
-                    cities: [
-                        { cname: 'Ottawa', code: 'C-OT' },
-                        { cname: 'Toronto', code: 'C-TO' }
-                    ]
-                }
-            ]
-        },
-        {
-            name: 'United States',
-            code: 'US',
-            states: [
-                {
-                    name: 'California',
-                    cities: [
-                        { cname: 'Los Angeles', code: 'US-LA' },
-                        { cname: 'San Diego', code: 'US-SD' },
-                        { cname: 'San Francisco', code: 'US-SF' }
-                    ]
-                },
-                {
-                    name: 'Florida',
-                    cities: [
-                        { cname: 'Jacksonville', code: 'US-JA' },
-                        { cname: 'Miami', code: 'US-MI' },
-                        { cname: 'Tampa', code: 'US-TA' },
-                        { cname: 'Orlando', code: 'US-OR' }
-                    ]
-                },
-                {
-                    name: 'Texas',
-                    cities: [
-                        { cname: 'Austin', code: 'US-AU' },
-                        { cname: 'Dallas', code: 'US-DA' },
-                        { cname: 'Houston', code: 'US-HO' }
-                    ]
-                }
-            ]
-        }
-    ];
-}   
-`
+    selectedCity: any;
+    
+    ngOnInit() {
+        this.countries = [
+            {
+                name: 'Australia',
+                code: 'AU',
+                states: [
+                    {
+                        name: 'New South Wales',
+                        cities: [
+                            { cname: 'Sydney', code: 'A-SY' },
+                            { cname: 'Newcastle', code: 'A-NE' },
+                            { cname: 'Wollongong', code: 'A-WO' }
+                        ]
+                    },
+                    {
+                        name: 'Queensland',
+                        cities: [
+                            { cname: 'Brisbane', code: 'A-BR' },
+                            { cname: 'Townsville', code: 'A-TO' }
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'Canada',
+                code: 'CA',
+                states: [
+                    {
+                        name: 'Quebec',
+                        cities: [
+                            { cname: 'Montreal', code: 'C-MO' },
+                            { cname: 'Quebec City', code: 'C-QU' }
+                        ]
+                    },
+                    {
+                        name: 'Ontario',
+                        cities: [
+                            { cname: 'Ottawa', code: 'C-OT' },
+                            { cname: 'Toronto', code: 'C-TO' }
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'United States',
+                code: 'US',
+                states: [
+                    {
+                        name: 'California',
+                        cities: [
+                            { cname: 'Los Angeles', code: 'US-LA' },
+                            { cname: 'San Diego', code: 'US-SD' },
+                            { cname: 'San Francisco', code: 'US-SF' }
+                        ]
+                    },
+                    {
+                        name: 'Florida',
+                        cities: [
+                            { cname: 'Jacksonville', code: 'US-JA' },
+                            { cname: 'Miami', code: 'US-MI' },
+                            { cname: 'Tampa', code: 'US-TA' },
+                            { cname: 'Orlando', code: 'US-OR' }
+                        ]
+                    },
+                    {
+                        name: 'Texas',
+                        cities: [
+                            { cname: 'Austin', code: 'US-AU' },
+                            { cname: 'Dallas', code: 'US-DA' },
+                            { cname: 'Houston', code: 'US-HO' }
+                        ]
+                    }
+                ]
+            }
+        ];
+    }
+}`
     };
 }
--- src/app/showcase/doc/cascadeselect/templatedoc.ts
+++ src/app/showcase/doc/cascadeselect/templatedoc.ts
@@ -109,7 +109,7 @@ export class TemplateDocComponent {
     }
 
     code: Code = {
-        html: `
+        basic: `
 <p-cascadeSelect [(ngModel)]="selectedCity" [options]="countries" optionLabel="cname" optionGroupLabel="name" [optionGroupChildren]="['states', 'cities']" [style]="{ minWidth: '14rem' }" placeholder="Select a City">
     <ng-template pTemplate="option" let-option>
         <div class="country-item">
@@ -120,87 +120,108 @@ export class TemplateDocComponent {
         </div>
     </ng-template>
 </p-cascadeSelect>`,
+
+        html: `
+<div class="card flex justify-content-center">
+    <p-cascadeSelect [(ngModel)]="selectedCity" [options]="countries" optionLabel="cname" optionGroupLabel="name" [optionGroupChildren]="['states', 'cities']" [style]="{ minWidth: '14rem' }" placeholder="Select a City">
+        <ng-template pTemplate="option" let-option>
+            <div class="country-item">
+                <img *ngIf="option.states" src="https://primefaces.org/cdn/primeng/images/demo/flag/flag_placeholder.png" [class]="'flag flag-' + option.code.toLowerCase()" />
+                <i class="pi pi-compass mr-2" *ngIf="option.cities"></i>
+                <i class="pi pi-map-marker mr-2" *ngIf="option.cname"></i>
+                <span>{{ option.cname || option.name }}</span>
+            </div>
+        </ng-template>
+    </p-cascadeSelect>
+</div>`,
+
         typescript: `
-countries: any[];
+import { Component } from '@angular/core';
 
-selectedCity: any;
+@Component({
+    templateUrl: './cascadeselectdemo.html'
+})
+export class CascadeSelectDemo {
+    countries: any[];
 
-ngOnInit() {
-    this.countries = [
-        {
-            name: 'Australia',
-            code: 'AU',
-            states: [
-                {
-                    name: 'New South Wales',
-                    cities: [
-                        { cname: 'Sydney', code: 'A-SY' },
-                        { cname: 'Newcastle', code: 'A-NE' },
-                        { cname: 'Wollongong', code: 'A-WO' }
-                    ]
-                },
-                {
-                    name: 'Queensland',
-                    cities: [
-                        { cname: 'Brisbane', code: 'A-BR' },
-                        { cname: 'Townsville', code: 'A-TO' }
-                    ]
-                }
-            ]
-        },
-        {
-            name: 'Canada',
-            code: 'CA',
-            states: [
-                {
-                    name: 'Quebec',
-                    cities: [
-                        { cname: 'Montreal', code: 'C-MO' },
-                        { cname: 'Quebec City', code: 'C-QU' }
-                    ]
-                },
-                {
-                    name: 'Ontario',
-                    cities: [
-                        { cname: 'Ottawa', code: 'C-OT' },
-                        { cname: 'Toronto', code: 'C-TO' }
-                    ]
-                }
-            ]
-        },
-        {
-            name: 'United States',
-            code: 'US',
-            states: [
-                {
-                    name: 'California',
-                    cities: [
-                        { cname: 'Los Angeles', code: 'US-LA' },
-                        { cname: 'San Diego', code: 'US-SD' },
-                        { cname: 'San Francisco', code: 'US-SF' }
-                    ]
-                },
-                {
-                    name: 'Florida',
-                    cities: [
-                        { cname: 'Jacksonville', code: 'US-JA' },
-                        { cname: 'Miami', code: 'US-MI' },
-                        { cname: 'Tampa', code: 'US-TA' },
-                        { cname: 'Orlando', code: 'US-OR' }
-                    ]
-                },
-                {
-                    name: 'Texas',
-                    cities: [
-                        { cname: 'Austin', code: 'US-AU' },
-                        { cname: 'Dallas', code: 'US-DA' },
-                        { cname: 'Houston', code: 'US-HO' }
-                    ]
-                }
-            ]
-        }
-    ];
-}   
-`
+    selectedCity: any;
+    
+    ngOnInit() {
+        this.countries = [
+            {
+                name: 'Australia',
+                code: 'AU',
+                states: [
+                    {
+                        name: 'New South Wales',
+                        cities: [
+                            { cname: 'Sydney', code: 'A-SY' },
+                            { cname: 'Newcastle', code: 'A-NE' },
+                            { cname: 'Wollongong', code: 'A-WO' }
+                        ]
+                    },
+                    {
+                        name: 'Queensland',
+                        cities: [
+                            { cname: 'Brisbane', code: 'A-BR' },
+                            { cname: 'Townsville', code: 'A-TO' }
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'Canada',
+                code: 'CA',
+                states: [
+                    {
+                        name: 'Quebec',
+                        cities: [
+                            { cname: 'Montreal', code: 'C-MO' },
+                            { cname: 'Quebec City', code: 'C-QU' }
+                        ]
+                    },
+                    {
+                        name: 'Ontario',
+                        cities: [
+                            { cname: 'Ottawa', code: 'C-OT' },
+                            { cname: 'Toronto', code: 'C-TO' }
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'United States',
+                code: 'US',
+                states: [
+                    {
+                        name: 'California',
+                        cities: [
+                            { cname: 'Los Angeles', code: 'US-LA' },
+                            { cname: 'San Diego', code: 'US-SD' },
+                            { cname: 'San Francisco', code: 'US-SF' }
+                        ]
+                    },
+                    {
+                        name: 'Florida',
+                        cities: [
+                            { cname: 'Jacksonville', code: 'US-JA' },
+                            { cname: 'Miami', code: 'US-MI' },
+                            { cname: 'Tampa', code: 'US-TA' },
+                            { cname: 'Orlando', code: 'US-OR' }
+                        ]
+                    },
+                    {
+                        name: 'Texas',
+                        cities: [
+                            { cname: 'Austin', code: 'US-AU' },
+                            { cname: 'Dallas', code: 'US-DA' },
+                            { cname: 'Houston', code: 'US-HO' }
+                        ]
+                    }
+                ]
+            }
+        ];
+    }
+}`
     };
 }
