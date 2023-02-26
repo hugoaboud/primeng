ea2643e0e24a7d55b44b9dc446debf70ec816784
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-02-26 14:08:48 +0300

    improve hashroute

--- src/app/showcase/layout/doc/docsectiontext/app.docsectiontext.component.ts
+++ src/app/showcase/layout/doc/docsectiontext/app.docsectiontext.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-docsectiontext',
@@ -12,10 +13,33 @@ export class AppDocSectionTextComponent {
 
     @Input() level: number = 2;
 
-    constructor(public router: Router){}
+    subscription!: Subscription;
 
-    navigate() {
+    constructor(public router: Router) {
+        this.subscription = this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+            const sub = event.url;
+            if (sub.includes('#')) {
+                const url = window.location.origin + sub;
+                navigator.clipboard.writeText(url);
+            }
+        });
+    }
+
+    navigate(event) {
         this.router.navigate([this.router.url.split('#')[0]], { fragment: this.id });
-        navigator.clipboard.writeText(window.location.href);
+        const parentElement = event.currentTarget.parentElement;
+        const hash = window.location.hash.substring(1);
+
+        setTimeout(() => {
+            parentElement.scrollIntoView({ block: 'start' });
+        }, 0);
+
+        hash === this.id && event.preventDefault();
+    }
+
+    ngOnDestroy() {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
     }
 }
