335d9e990ebe1347a824b11befd104e73c7ff18b
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-03-01 14:05:13 +0300

    refactor

--- src/app/showcase/doc/installation/videos/angular-clidoc.ts
+++ src/app/showcase/doc/installation/videos/angular-clidoc.ts
@@ -4,7 +4,7 @@ import { Component, Input } from '@angular/core';
     selector: 'angular-cli-doc',
     template: ` <div>
         <app-docsectiontext [title]="title" [id]="id">
-            <p>Angular CLI is the official CLI tool for Angular to create Angular Apps. We strongly suggest using Angular CLI when starting an Angular project.</p>
+            <p><i>Angular CLI</i> is the official CLI tool for Angular to create Angular Apps. We strongly suggest using Angular CLI when starting an Angular project.</p>
         </app-docsectiontext>
         <div class="video-container">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/pkOx7bSBQsg" frameborder="0" allowfullscreen></iframe>
--- src/app/showcase/doc/installation/videos/videosdoc.ts
+++ src/app/showcase/doc/installation/videos/videosdoc.ts
@@ -6,9 +6,6 @@ import { Component, Input } from '@angular/core';
         <app-docsectiontext [title]="title" [id]="id">
             <p>Video tutorials to take you through step-by-step.</p>
         </app-docsectiontext>
-        <div class="card flex justify-content-center">
-            <p-button label="Check" icon="pi pi-check"></p-button>
-        </div>
     </div>`
 })
 export class VideosDocComponent {
