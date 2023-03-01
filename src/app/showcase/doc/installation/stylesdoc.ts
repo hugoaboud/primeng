e95d128c79dbb2b432e59c49a44aa4c321ce096a
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-03-01 10:01:24 +0300

    update text

--- src/app/showcase/doc/installation/stylesdoc.ts
+++ src/app/showcase/doc/installation/stylesdoc.ts
@@ -5,7 +5,7 @@ import { Code } from '../../domain/code';
     selector: 'styles-doc',
     template: ` <div>
         <app-docsectiontext [title]="title" [id]="id">
-        <p>The css dependencies are as follows, Prime Icons, theme of your choice and structural css of components. In <i>angular.json</i> file find the <i>styles</i> array and add the following:</p>
+        <p>Theme, core and icons are the necessary css files of the components, visit the <a href="/theming#themes" class="">Themes</a> section for the complete list of available themes to choose from. The css dependencies are as follows, Prime Icons, theme of your choice and structural css of components. In <i>angular.json</i> file find the <i>styles</i> array and add the following:</p>
         </app-docsectiontext>
         <app-code [code]="code" [hideToggleCode]="true"></app-code>
     </div>`
