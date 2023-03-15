6e8ab6492a217eba079f757ac1585e7518eb54df
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-03-15 15:48:35 +0300

    Add scroll to active item on init

--- src/app/showcase/layout/menu/app.menu.component.ts
+++ src/app/showcase/layout/menu/app.menu.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { DomHandler } from 'primeng/dom';
 import { Subscription } from 'rxjs';
 import { default as MenuData } from 'src/assets/showcase/data/menu.json';
 import { AppConfig } from '../../domain/appconfig';
@@ -20,22 +22,42 @@ export interface MenuItem {
 export class AppMenuComponent {
     @Input() active: boolean;
 
-    menu: MenuItem[];
+    menu!: MenuItem[];
 
-    config: AppConfig;
+    config!: AppConfig;
 
-    subscription: Subscription;
+    subscription!: Subscription;
 
-    constructor(private configService: AppConfigService) {
+    routeSubscription!: Subscription;
+
+    constructor(private configService: AppConfigService, private el: ElementRef, private router: Router) {
         this.menu = MenuData.data;
 
         this.config = this.configService.config;
         this.subscription = this.configService.configUpdate$.subscribe((config) => (this.config = config));
+        this.routeSubscription = this.router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                setTimeout(() => {
+                    this.scrollToActiveItem();
+                }, 1);
+            }
+        })
+    }
+
+    scrollToActiveItem() {
+        let activeItem = DomHandler.findSingle(this.el.nativeElement, '.router-link-active')
+        if (activeItem) {
+            activeItem.scrollIntoView({inline: 'start', behavior: 'smooth'})
+        }
     }
 
     ngOnDestroy() {
         if (this.subscription) {
             this.subscription.unsubscribe();
+        }
+
+        if (this.routeSubscription) {
+            this.routeSubscription.unsubscribe();
         }
     }
 }
--- src/app/showcase/layout/menu/app.menuitem.component.ts
+++ src/app/showcase/layout/menu/app.menuitem.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { MenuItem } from './app.menu.component';
 
 @Component({
@@ -9,4 +10,11 @@ export class AppMenuItemComponent {
     @Input() item: MenuItem;
 
     @Input() root: boolean = true;
+
+    constructor(private router: Router) {}
+
+    isActiveRootMenuItem(menuitem: MenuItem): boolean {
+        return menuitem.children && !menuitem.children.some((item) => item.routerLink === `${this.router.url}` || (item.children && item.children.some((it) => it.routerLink === `${this.router.url}`)));
+    }
+
 }
